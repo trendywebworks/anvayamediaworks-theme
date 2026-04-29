110a1d33d3737a4085380262e465059199eae8ae
trendywebworks <trendywebworks@gmail.com>
2026-04-29 15:54:34 +0530

    style fix

--- functions.php
+++ functions.php
@@ -175,5 +175,17 @@ function validate_indian_phone_cf7($result, $tag) {
     return $result;
 }
 
+add_action('wp_enqueue_scripts', function () {
+    if (is_page('contact')) {
+        if (function_exists('wpcf7_enqueue_scripts')) {
+            wpcf7_enqueue_scripts();
+        }
+
+        if (function_exists('wpcf7_enqueue_styles')) {
+            wpcf7_enqueue_styles();
+        }
+    }
+}, 20);
+
 
 ?>
